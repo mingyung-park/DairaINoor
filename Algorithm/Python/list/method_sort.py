e13abea5d22cc0b19ddb9785224b604057990360
Min Gyung Park <kateking001130@ewhain.net>
2023-08-23 17:33:55 +0900

    categorize python list mehtod

---
+++ Algorithm/Python/list/method_sort.py
@@ -0,0 +1,14 @@
+# Reference : https://docs.python.org/3/tutorial/datastructures.html
+
+# Methods of list
+
+#8: list.sort(*, key=None, reverse=False)
+#   Sort the items of the list in place 
+#   (the arguments can be used for sort customization, see sorted() for their explanation).
+#   key
+
+list=[3,2,4,1,5]
+list.sort()
+list.sort(reverse=True)
+list.sort(key = lambda x:(x%2),reverse=True)
+print(list)
